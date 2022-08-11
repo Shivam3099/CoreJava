8aaae8ccc3d5940011e6baf75663cf3e573a77e5
Shivam3099 <99005227+Shivam3099@users.noreply.github.com>
2022-08-11 12:47:51 +0530

    Update SmallerNo.java

--- JavaBasics/Exercise/SmallerNo.java
+++ JavaBasics/Exercise/SmallerNo.java
@@ -6,11 +6,13 @@ public static void main(String[] args) {
 
 	int a = 40;
 	int b = 30;
+	int c = 20;
 	
 	if(a<b) {
 		System.out.println("a  is smaller");
 	}else {
 	System.out.println("a is gretter");
-	}
+	}else {
+		system.out.println("c value")
 }
 }
